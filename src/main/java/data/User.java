ed3ee185aa455c063c177cfe33cdfcd455df06c0
Svet M <67125915+securesvet@users.noreply.github.com>
2022-10-31 13:20:20 +0500

    Update User.java

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -1,9 +1,18 @@
 package data;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
-//    private String username;
+    //    private String username;
 //    private String photo;
 //    private Long chatId;
+    private int id;
+    @Column(name = "name")
+    private String name;
+    private int age;
+    private long photo;
     private Boolean statusOfWaitingPhoto;
     private Boolean statusOfWaitingName;
 
@@ -11,12 +20,32 @@ public class User {
 //        this.username = username;
 //        this.chatId = chatId;
 //        this.photo = photo;
-        this.statusOfWaitingName=statusOfWaitingName;
-        this.statusOfWaitingPhoto=statusOfWaitingPhoto;
+        this.statusOfWaitingName = statusOfWaitingName;
+        this.statusOfWaitingPhoto = statusOfWaitingPhoto;
     }
-    public boolean getStatusOfPhoto(){return statusOfWaitingPhoto;}
-    public boolean getStatusOfName(){return statusOfWaitingName;}
-    public void setStatusOfPhoto(Boolean newStatusOfPhoto){statusOfWaitingPhoto=newStatusOfPhoto;}
-    public void setStatusOfName(Boolean newStatusOfName){statusOfWaitingName=newStatusOfName;}
+
+    public boolean getStatusOfPhoto() {
+        return statusOfWaitingPhoto;
+    }
+
+    public boolean getStatusOfName() {
+        return statusOfWaitingName;
+    }
+
+    public void setStatusOfPhoto(Boolean newStatusOfPhoto) {
+        statusOfWaitingPhoto = newStatusOfPhoto;
+    }
+
+    public void setStatusOfName(Boolean newStatusOfName) {
+        statusOfWaitingName = newStatusOfName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
 }
